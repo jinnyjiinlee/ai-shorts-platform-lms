6b3832250b7477967bed06ddd62fdfb49d0c0777
jinny <ggab.challenge@gmail.com>
2025-08-23 06:34:21 +0900

    feat(admin): create user management and approval system

---
+++ src/app/admin/usersManagement/page.tsx
@@ -0,0 +1,5 @@
+import UserRegistrationManagement from '../../components/admin/users/UserRegistrationManagement';
+
+export default function UsersPage() {
+  return <UserRegistrationManagement />;
+}
